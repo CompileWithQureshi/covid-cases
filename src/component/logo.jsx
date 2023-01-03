d67bb727dbbbe8101cb871daf2c2bae58992f318
contactme.qureshi@gmail.com <contactme.qureshi@gmail.com>
2023-01-03 02:49:13 -0800

    Added animation on logo

--- src/component/logo.jsx
+++ src/component/logo.jsx
@@ -1,11 +1,18 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import "../App.css";
 
 export const Logo = () => {
+  const [rotate, setRotate] = useState(false);
   return (
-    <div className="logo-animate">
+    <motion.div
+      className="logo-animate"
+      animate={{ rotate: rotate ? 0 : 360 }}
+      transition={{ type: "tween", duration: 3 }}
+      whileDrag={{ x: 50, y: 50 }}
+      onClick={() => setRotate(!rotate)}
+    >
       <h5 className="logo-name">Covid 19</h5>
-      <motion.div animate={{ y: -100 }}></motion.div>
-    </div>
+    </motion.div>
   );
 };
